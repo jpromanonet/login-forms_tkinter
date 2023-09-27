bd157a890501a810012c90584d23472551e81ed8
jpromanonet <programming@jpromano.net>
2023-09-27 20:50:15 -0300

    Working on login component

--- main.py
+++ main.py
@@ -17,7 +17,11 @@ Label(root, image=login_img, bg="white").place(x=50, y=50)
 frame = Frame(root, width = 350, height = 350, bg = "red")
 frame.place(x = 480, y = 70)
 
-heading = Label(frame, text = "Sign In", fg = "#57a1f8")
+heading = Label(frame, text = "Sign In", fg = "#57a1f8", bg='white', font=('Microsoft YaHei UI Light',23,'bold'))
+heading.place(x=100,y=5)
+
+user = Entry(frame, width=25,fg='black',border=2,bg="white", font=('Microsoft YaHei UI Light',11))
+user.place(x=30,y=80)
 
 # Running the app
 root.mainloop()
